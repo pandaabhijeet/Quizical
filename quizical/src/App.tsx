cc8d4ce57959cc35a0939caf488160514194f8bb
chronicle <abhijeetpanda06@gmail.com>
2024-01-02 08:57:26 +0530

    logic and ui

--- quizical/src/App.tsx
+++ quizical/src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
+  const [Loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
-    <div className="App"> QUIZICAL
+    <div className="App">
+      <h1>QUIZICAL</h1>
+      <button className="startQuiz" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard
+        questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswer ? userAnswer[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
